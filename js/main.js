464c96043763dadba6b33af2a84205e2783fa546
vetik32 <vetik32@yahoo.com>
2012-11-02 00:44:22 +0200

    0.1.1 added node-server.js which returns markup with ids sent using ajax call

--- js/main.js
+++ js/main.js
@@ -1,22 +1,64 @@
 var app = app || {};
 
+app.debugMode = false;
 
-app = (function ($draggable) {
-  var
+app = (function () {
+  "use strict";
+  var internalDNDType = 'text/x-dnd-json', // set this to something specific to your source: http://www.w3.org/TR/html5/dnd.html#dnd
     hoverClass = "-hover",
-    draggingClass = "-dragging";
+    selectedClass = "-selected",
+    draggingClass = "-dragging",
+    hiddenClass = "-hidden",
+    helperClasses = [selectedClass, hoverClass, draggingClass],
+    responseTmpl = "item(s) with id(s): [{1}] successfully moved!";
 
-  $(document.body ).bind( "newItem", function(event, newItem){
-     app.makeDraggable(newItem);
+  $(document.body ).bind( "newItemsLoaded.alertBox", function(event, data){
+    var responseText = responseTmpl.replace("{1}", data.ids);
+     $("#alertBox" ).find(".-response" ).text( responseText );
+     $("#alertBox" ).fadeIn();
   });
 
+  var selectOnlyOne = function( item ){
+    $( item ).siblings().removeClass( selectedClass );
+    $( item ).addClass( selectedClass );
+  };
+
+  var resetStyle = function( item ){
+    helperClasses.forEach(function(className){
+      $( item ).removeClass( className );
+    });
+  };
+
+  var getDraggedItems = function( $draggableSelector ){
+    return $( $draggableSelector ).filter(function(){
+        return $(this ).hasClass( draggingClass)
+          || $( this ).hasClass( selectedClass );
+      });
+  };
+
+  var _log = function( message ) {
+    if ( console && app.debugMode ) {
+      console.log( message );
+    }
+  };
+
   return {
-    makeDraggable: function ( $draggable ) {
-      $( $draggable )
-        .bind( "mouseenter", function () {
+    makeDraggable: function ( $draggableSelector ) {
+      $( $draggableSelector )
+        .bind( "mouseup.dnd", function( ev ){
+          if ( ev.ctrlKey ) {
+            if ($( this ).hasClass( selectedClass )) {
+              _log("removing");
+            }
+            $( this ).toggleClass( selectedClass );
+          } else {
+            selectOnlyOne( this );
+          }
+        })
+        .bind( "mouseenter.dnd", function () {
           $( this ).addClass( hoverClass );
         })
-        .bind( "mouseleave", function () {
+        .bind( "mouseleave.dnd", function () {
           $( this ).removeClass( hoverClass );
         })
 
@@ -24,65 +66,112 @@ app = (function ($draggable) {
         .attr( "draggable", "true" )
 
         // Handle the start of dragging to initialize.
-        .bind( "dragstart", function (ev) {
-          // dragged property works for drops inside the root doc.
-          $( this ).css( "opacity", .4 );
-          $node = $( ev.target ).addClass( draggingClass );
+        .bind( "dragstart.dnd", function ( jqueryEvent ) {
+          var event = jqueryEvent.originalEvent,
+            dataTransfer = event.dataTransfer;
 
-          // Set text & HTML content for drops outside the root doc.
-          var dt = ev.originalEvent.dataTransfer;
-          dt.setData( "text/html", $node.html() );
-          dt.setData( "text/plain", $node.text() );
-          return true;
+          if ( !event.target instanceof HTMLLIElement ) {
+            event.preventDefault(); // don't allow selection to be dragged
+          }
+
+          $( this ).addClass( draggingClass );
+
+          var ids = [],
+            $selectedItems = getDraggedItems( $draggableSelector );
+
+          if ( $selectedItems.size() === 0) {
+            ids.push( event.target.dataset.id );
+          }
+
+          $selectedItems.each(function(){
+            $( this ).addClass( draggingClass );
+            ids.push( this.dataset.id );
+          });
+
+          var myjson = {
+            "origin" : internalDNDType,
+            "ids" : ids.join( "," )
+          };
+
+          dataTransfer.setData( internalDNDType, JSON.stringify(myjson));
+          dataTransfer.effectAllowed = 'move'; // only allow moves
         })
 
         // Handle the end of dragging.
-        .bind( "dragend", function (ev) {
-          // $.log( "#newschool .messages", "Drag ended" );
-          $( this ).css( "opacity", 1 )
-            .removeClass( "-dragging -hover" );
-
+        .bind( "dragend.dnd", function (ev) {
           var dt = ev.originalEvent.dataTransfer;
           if ( dt.dropEffect === "move" ) {
-            $( this ).remove()
+            _log(getDraggedItems( $draggableSelector ));
+            getDraggedItems( $draggableSelector ).each(function(){
+              $( this ).remove();
+              if ( $( $draggableSelector ).size() === 1) {
+                $( $draggableSelector ).filter(".placeholder").removeClass( hiddenClass )
+              }
+            });
           }
+          resetStyle(this);
           return false;
         });
     },
 
     makeDroppable: function ($droppable) {
+
+      $(document.body ).bind( "newItemsLoaded.dropppableContainer", function(event, data){
+         $( $droppable ).prepend( data.response );
+      });
+
       $( $droppable )
-        .bind( "dragenter", function (ev) {
+        .bind( "dragenter.dnd", function () {
           $( this ).addClass( hoverClass );
           return false;
         })
 
         // Un-highlight on drag leaving drop zone.
-        .bind( "dragleave", function (ev) {
+        .bind( "dragleave.dnd", function () {
           $( this ).removeClass( hoverClass );
           return false;
         })
 
         // Decide whether the thing dragged in is welcome.
-        .bind( "dragover", function (ev) {
+        .bind( "dragover.dnd", function (ev) {
+          ev.originalEvent.dataTransfer.dropEffect = 'move';
           return false;
         })
 
         // Handle the final drop...
-        .bind( "drop", function (ev) {
-          $( this ).removeClass( hoverClass );
+        .bind( "drop.dnd", function (jqueryEvent) {
+          var event = jqueryEvent.originalEvent,
+            rowData = event.dataTransfer.getData( internalDNDType ),
+            data;
 
-          if ( ev.stopPropagation ) {
-            ev.stopPropagation(); // Stops some browsers from redirecting.
+            try {
+              // to avoid dropping "bad" content such "<script>...</script>"
+              data = JSON.parse(rowData);
+            } catch (e ) {
+              _log(e);
+              return;
+            }
+
+            if (data.origin != internalDNDType ) {
+              _log("sorry");
+              return;
+            }
+
+            if ( event.stopPropagation ) {
+              event.stopPropagation(); // Stops some browsers from redirecting.
+            }
+
+            $.ajax({
+              "url" : "/getNewItems",
+              "data" : {
+                "ids" : data.ids
+              },
+              "success": function( response ){
+                $( document.body ).trigger( "newItemsLoaded", [{ "ids": data.ids, "response" : response }] );
+              }
+            })
           }
-          var dt = ev.originalEvent.dataTransfer,
-            data = (dt.getData( "text/plain" ));
-
-          $( this ).prepend( "<li>" + data + "</li>" );
-          $(document.body ).trigger( "newItem", $( this ).find("li:first") );
-          return false;
-        });
-
+        );
     }
   }
 })();
---
+++ node-server.js
@@ -0,0 +1,60 @@
+
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8888;
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
+
+  if ( uri === "/getNewItems" ) {
+    var _get = url.parse(request.url, true).query;
+     // Write headers to the response.
+     response.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "text/plain"
+     });
+     // Send data and end response.
+     var idsParam = _get['ids'],
+       markup = "";
+       if (idsParam) {
+         var ids = idsParam.split(",");
+         for (var i = 0; i < ids.length; i+= 1) {
+           var id = ids[i];
+           if (id) {
+             markup += '<li data-id="' + id + '"> : Item ' + id + '</li>\n';
+           }
+         }
+       }
+     response.end(markup);
+    return;
+  }
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+  if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
